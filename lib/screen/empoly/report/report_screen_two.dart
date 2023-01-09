9d5dfe71889dc4aefd8e3a93291f5464f4159656
lahcenegue <lahcene90.lg@gmail.com>
2023-01-09 17:58:50 +0100

    update 2

--- lib/screen/empoly/report/report_screen_two.dart
+++ lib/screen/empoly/report/report_screen_two.dart
@@ -57,6 +57,7 @@ class ReportScreenTwo extends StatelessWidget {
                           height: 50,
                         ),
                         Container(
+                          padding: const EdgeInsets.all(15.0),
                           decoration: const BoxDecoration(
                             color: Colors.white,
                             borderRadius: BorderRadius.only(
@@ -64,159 +65,146 @@ class ReportScreenTwo extends StatelessWidget {
                               topRight: Radius.circular(25),
                             ),
                           ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: Column(children: [
-                              steps(true, true, false),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              (state is LoadingGetMosques &&
-                                      cubit.mosquesName.isEmpty)
-                                  ? const CircularProgressIndicator()
-                                  : DropdownButtonFormField(
-                                      isExpanded: true,
-                                      style:
-                                          const TextStyle(color: Colors.black),
-                                      dropdownColor: Colors.white,
-                                      decoration: InputDecoration(
-                                        fillColor: Colors.white,
-                                        filled: true,
-                                        border: const OutlineInputBorder(),
-                                        focusedBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(
-                                              color: Colors.grey.shade200,
-                                            ),
-                                            borderRadius:
-                                                const BorderRadius.all(
-                                                    Radius.circular(10.0))),
-                                        enabledBorder: OutlineInputBorder(
-                                            borderSide: BorderSide(
-                                                color: Colors.grey.shade200),
-                                            borderRadius:
-                                                const BorderRadius.all(
-                                                    Radius.circular(10.0))),
-                                      ),
-                                      items: [
-                                        ...cubit.mosquesName.map((e) {
-                                          return DropdownMenuItem(
-                                            onTap: () {},
-                                            value: e,
-                                            child: Text(
-                                              e,
-                                              style: const TextStyle(
-                                                  fontWeight: FontWeight.bold),
-                                            ),
-                                          );
-                                        }),
-                                      ],
-                                      value: mosque,
-                                      onChanged: (val) {
-                                        mosque = val.toString();
-                                        cubit.getDateMosque(
-                                            mosqueName: mosque!);
-                                      }),
-                              (state is LoadingGetDataMosque)
-                                  ? const CircularProgressIndicator()
-                                  : Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      children: [
-                                        showDate(
-                                            name: 'الــمــؤذن',
-                                            date: cubit.mosqueDate == null
-                                                ? ''
-                                                : cubit.mosqueDate!.moazin),
-                                        showDate(
-                                            name:
-                                                'رقـــم الــجــوال الــمــؤذن',
-                                            date: cubit.mosqueDate == null
-                                                ? ''
-                                                : cubit
-                                                    .mosqueDate!.moazin_phone),
-                                        showDate(
-                                            name: 'الإمـــام',
-                                            date: cubit.mosqueDate == null
-                                                ? ''
-                                                : cubit.mosqueDate!.imam),
-                                        showDate(
-                                            name: 'رقـــم الــجــوال الإمـــام',
-                                            date: cubit.mosqueDate == null
-                                                ? ''
-                                                : cubit.mosqueDate!.imam_phone),
-                                        showDate(
-                                            name: 'الـــخــطـيـب',
-                                            date: cubit.mosqueDate == null
-                                                ? ''
-                                                : cubit.mosqueDate!.kahteeb),
-                                        showDate(
-                                            name:
-                                                'رقـــم الــجــوال الـــخــطـيـب',
-                                            date: cubit.mosqueDate == null
-                                                ? ''
-                                                : cubit
-                                                    .mosqueDate!.kahteeb_phone),
-                                        showDate(
-                                            name: 'الـــخادم',
-                                            date: cubit.mosqueDate == null
-                                                ? ''
-                                                : cubit
-                                                    .mosqueDate!.servant_name),
-                                        showDate(
-                                            name: 'رقـــم الــجــوال الـــخادم',
-                                            date: cubit.mosqueDate == null
-                                                ? ''
-                                                : cubit.mosqueDate!
-                                                    .servant_number),
-                                        showDate(
-                                            name: 'الحي',
-                                            date: cubit.mosqueDate == null
-                                                ? ''
-                                                : cubit.mosqueDate!
-                                                    .neighborhood_name),
-                                      ],
+                          child: Column(children: [
+                            steps(true, true, false),
+                            const SizedBox(height: 10),
+                            (state is LoadingGetMosques &&
+                                    cubit.mosquesName.isEmpty)
+                                ? const CircularProgressIndicator()
+                                : DropdownButtonFormField(
+                                    isExpanded: true,
+                                    style: const TextStyle(color: Colors.black),
+                                    dropdownColor: Colors.white,
+                                    decoration: InputDecoration(
+                                      fillColor: Colors.white,
+                                      filled: true,
+                                      border: const OutlineInputBorder(),
+                                      focusedBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                            color: Colors.grey.shade200,
+                                          ),
+                                          borderRadius: const BorderRadius.all(
+                                              Radius.circular(10.0))),
+                                      enabledBorder: OutlineInputBorder(
+                                          borderSide: BorderSide(
+                                              color: Colors.grey.shade200),
+                                          borderRadius: const BorderRadius.all(
+                                              Radius.circular(10.0))),
                                     ),
-                              const SizedBox(
-                                height: 15,
-                              ),
-                              InkWell(
-                                onTap: () {
-                                  if (cubit.mosqueDate != null &&
-                                      mosque != null) {
-                                    navigatorTo(
-                                        context: context,
-                                        widget: ReportScreenThree(
-                                          address: address,
-                                          createdAt: createdAt,
-                                          lat: lat,
-                                          lng: lng,
-                                          mosqueDate: cubit.mosqueDate!,
-                                        ));
-                                  }
-                                },
-                                child: Container(
-                                  alignment: Alignment.center,
-                                  constraints: const BoxConstraints(
-                                      maxHeight: 50, maxWidth: 320),
-                                  decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(15),
-                                      gradient: const LinearGradient(
-                                          begin: Alignment.topCenter,
-                                          end: Alignment.bottomCenter,
-                                          colors: [
-                                            Color(0xFFC4D35F),
-                                            Color(0xFF578B37),
-                                          ])),
-                                  child: const Text(
-                                    "الـــتــــالي ",
-                                    style: TextStyle(color: Colors.white),
+                                    items: [
+                                      ...cubit.mosquesName.map((e) {
+                                        return DropdownMenuItem(
+                                          onTap: () {},
+                                          value: e,
+                                          child: Text(
+                                            e,
+                                            style: const TextStyle(
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        );
+                                      }),
+                                    ],
+                                    value: mosque,
+                                    onChanged: (val) {
+                                      mosque = val.toString();
+                                      cubit.getDateMosque(mosqueName: mosque!);
+                                    }),
+                            (state is LoadingGetDataMosque)
+                                ? const CircularProgressIndicator()
+                                : Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      showDate(
+                                          name: 'الــمــؤذن',
+                                          date: cubit.mosqueDate == null
+                                              ? ''
+                                              : cubit.mosqueDate!.moazin),
+                                      showDate(
+                                          name: 'رقـــم الــجــوال الــمــؤذن',
+                                          date: cubit.mosqueDate == null
+                                              ? ''
+                                              : cubit.mosqueDate!.moazin_phone),
+                                      showDate(
+                                          name: 'الإمـــام',
+                                          date: cubit.mosqueDate == null
+                                              ? ''
+                                              : cubit.mosqueDate!.imam),
+                                      showDate(
+                                          name: 'رقـــم الــجــوال الإمـــام',
+                                          date: cubit.mosqueDate == null
+                                              ? ''
+                                              : cubit.mosqueDate!.imam_phone),
+                                      showDate(
+                                          name: 'الـــخــطـيـب',
+                                          date: cubit.mosqueDate == null
+                                              ? ''
+                                              : cubit.mosqueDate!.kahteeb),
+                                      showDate(
+                                          name:
+                                              'رقـــم الــجــوال الـــخــطـيـب',
+                                          date: cubit.mosqueDate == null
+                                              ? ''
+                                              : cubit
+                                                  .mosqueDate!.kahteeb_phone),
+                                      showDate(
+                                          name: 'الـــخادم',
+                                          date: cubit.mosqueDate == null
+                                              ? ''
+                                              : cubit.mosqueDate!.servant_name),
+                                      showDate(
+                                          name: 'رقـــم الــجــوال الـــخادم',
+                                          date: cubit.mosqueDate == null
+                                              ? ''
+                                              : cubit
+                                                  .mosqueDate!.servant_number),
+                                      showDate(
+                                          name: 'الحي',
+                                          date: cubit.mosqueDate == null
+                                              ? ''
+                                              : cubit.mosqueDate!
+                                                  .neighborhood_name),
+                                    ],
                                   ),
+                            const SizedBox(
+                              height: 15,
+                            ),
+                            InkWell(
+                              onTap: () {
+                                if (cubit.mosqueDate != null &&
+                                    mosque != null) {
+                                  navigatorTo(
+                                      context: context,
+                                      widget: ReportScreenThree(
+                                        address: address,
+                                        createdAt: createdAt,
+                                        lat: lat,
+                                        lng: lng,
+                                        mosqueDate: cubit.mosqueDate!,
+                                      ));
+                                }
+                              },
+                              child: Container(
+                                alignment: Alignment.center,
+                                constraints: const BoxConstraints(
+                                    maxHeight: 50, maxWidth: 320),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(15),
+                                    gradient: const LinearGradient(
+                                        begin: Alignment.topCenter,
+                                        end: Alignment.bottomCenter,
+                                        colors: [
+                                          Color(0xFFC4D35F),
+                                          Color(0xFF578B37),
+                                        ])),
+                                child: const Text(
+                                  "الـــتــــالي ",
+                                  style: TextStyle(color: Colors.white),
                                 ),
                               ),
-                            ]),
-                          ),
+                            ),
+                          ]),
                         )
                       ],
                     ),
